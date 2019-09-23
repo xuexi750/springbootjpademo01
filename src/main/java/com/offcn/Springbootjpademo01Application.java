583143ed8928bdf8c987eef39676ba1e44c03e35
eunny <test@qq.com>
2019-09-23 17:26:24 +0800

    修改主启动类   新增中文注释

--- src/main/java/com/offcn/Springbootjpademo01Application.java
+++ src/main/java/com/offcn/Springbootjpademo01Application.java
@@ -2,7 +2,7 @@ package com.offcn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//新增中文注释,,测试用
 @SpringBootApplication
 public class Springbootjpademo01Application {
 
